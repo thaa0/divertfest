ccbc26899e50f5485b65f3143866f3fc0bde3bfc
Thalita Souza <dev.thalita@gmail.com>
2024-11-25 20:06:12 -0300

    inicia projeto

---
+++ src/main/java/br/com/divertech/divertfest/DivertfestApplication.java
@@ -0,0 +1,13 @@
+package br.com.divertech.divertfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivertfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivertfestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/divertech/divertfest/DivertfestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.divertech.divertfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivertfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
