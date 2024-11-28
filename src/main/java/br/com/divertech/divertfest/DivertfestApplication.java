e1e805420b30ccba96af7e910bf51cba60570925
Thalita Souza <dev.thalita@gmail.com>
2024-11-27 22:27:41 -0300

    feat:implementa docker

--- src/main/java/br/com/divertech/divertfest/DivertfestApplication.java
+++ src/main/java/br/com/divertech/divertfest/DivertfestApplication.java
@@ -2,12 +2,21 @@ package br.com.divertech.divertfest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/home")
 public class DivertfestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DivertfestApplication.class, args);
 	}
-
+	
+	@GetMapping
+	public String testeHome() {
+		return "Bem vindo ao Divertfest!";
+	}
 }
