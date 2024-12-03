7ee8c09345cf3fddd4e598dfcc15ec06bbb64717
Thalita Souza <dev.thalita@gmail.com>
2024-12-02 23:18:43 -0300

    feat:implementa dominio de brinquedo

---
+++ src/main/java/br/com/divertech/divertfest/brinquedo/domain/Brinquedo.java
@@ -0,0 +1,29 @@
+package br.com.divertech.divertfest.brinquedo.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+public class Brinquedo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true,nullable = false)
+    private UUID idBrinquedo;
+    @NotBlank(message = "O campo Nome não deve estar em branco")
+    private String nome;
+    @NotBlank
+    private String descricao;
+    @NotBlank
+    private String categoria;
+    @Enumerated(EnumType.STRING)
+    private StatusBrinquedo status;
+    @NotBlank
+    private String imagem;
+}
