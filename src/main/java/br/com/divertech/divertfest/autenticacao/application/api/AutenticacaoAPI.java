a66dea514de6a82c4a24016fd3d9ded1501468df
Thalita Souza <dev.thalita@gmail.com>
2024-12-06 22:59:03 -0300

    refactor:atualiza swgger

--- src/main/java/br/com/divertech/divertfest/autenticacao/application/api/AutenticacaoAPI.java
+++ src/main/java/br/com/divertech/divertfest/autenticacao/application/api/AutenticacaoAPI.java
@@ -1,5 +1,7 @@
 package br.com.divertech.divertfest.autenticacao.application.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,13 @@ import javax.validation.Valid;
 
 @RequestMapping("/public/v1/autenticacao")
 public interface AutenticacaoAPI {
+
+    @Operation(summary = "Autenticação do usuário",
+            description = "Realiza a autenticação do usuário com nome de usuário e senha.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Autenticação bem-sucedida"),
+                    @ApiResponse(responseCode = "401", description = "Usuário ou senha inválidos")
+            })
     @PostMapping("/autentica")
     @ResponseStatus(HttpStatus.OK)
     TokenResponse autentica(@RequestBody @Valid AutenticacaoRequest autenticacaoRequest) throws AuthenticationException;
--- src/main/java/br/com/divertech/divertfest/autenticacao/application/api/AutenticacaoRequest.java
+++ src/main/java/br/com/divertech/divertfest/autenticacao/application/api/AutenticacaoRequest.java
@@ -5,6 +5,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import lombok.Builder;
@@ -13,15 +15,17 @@ import lombok.Data;
 @Data
 @Builder
 public class AutenticacaoRequest {
-    @NotNull
+    @Schema(description = "E-mail do usuário", example = "usuario@exemplo.com")
     @NotBlank(message = "O usuário não pode estar em branco!")
     @Column(unique = true)
     @Email
     private String usuario;
+    @Schema(description = "Senha do usuário", example = "senha123")
     @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
     @NotNull
     private String senha;
 
+    @JsonIgnore
     public UsernamePasswordAuthenticationToken getUserPassToken() {
         return new UsernamePasswordAuthenticationToken(usuario, senha);
     }
