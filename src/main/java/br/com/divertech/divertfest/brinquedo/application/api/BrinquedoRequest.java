793a22af5d4c5a95948e7c12c43d4f0d5d82af9f
Thalita Souza <dev.thalita@gmail.com>
2024-12-09 19:56:03 -0300

    feat: implementa request do metodo cadastraBrinquedo

--- src/main/java/br/com/divertech/divertfest/brinquedo/application/api/BrinquedoRequest.java
+++ src/main/java/br/com/divertech/divertfest/brinquedo/application/api/BrinquedoRequest.java
@@ -1,4 +1,16 @@
 package br.com.divertech.divertfest.brinquedo.application.api;
 
+import br.com.divertech.divertfest.brinquedo.domain.Categoria;
+import br.com.divertech.divertfest.brinquedo.domain.StatusBrinquedo;
+import br.com.divertech.divertfest.locador.domain.Locador;
+
+import java.util.UUID;
+
 public class BrinquedoRequest {
+    private String nome;
+    private String descricao;
+    private Categoria categoria;
+    private StatusBrinquedo status;
+    private String imagem;
+    private UUID donoBrinquedo;
 }
